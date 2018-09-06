0e48aaff31b0ee626e3a2ae507af953658dbcd85
Karthikeya Subbarao <karthikeya.subbarao@valtech.de>
2018-09-06 17:10:43 +0200

    docker file fix

--- deploy/docker/docker_build_ros.sh
+++ deploy/docker/docker_build_ros.sh
@@ -1,1 +1,1 @@
-docker build -t fluxproject/ros_base deploy/docker/ros_base/Dockerfile deploy/docker/ros_base
+docker build -t fluxproject/ros_base -f deploy/docker/ros_base/Dockerfile deploy/docker/ros_base
